9f230b1ac7cb68a97db97a4e9333ceec779a014c
BushraZehraAijaz <160467770+BushraZehraAijaz@users.noreply.github.com>
2024-05-30 19:32:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+let c_value = {
+    PKR: 279.72,
+    CAD: 0.73,
+    USD: 1
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "Converting from",
+        choices: ['PKR', 'CAD', 'USD']
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "Converting to",
+        choices: ['PKR', 'CAD', 'USD']
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Your amount to convert"
+    }
+]);
+console.log(c_value[ans.to] / c_value[ans.from] * ans.amount);
